c10df840d74085981e9c4800e0195ffdd93a5025
Yanorei32 <yanorei@hotmail.co.jp>
2018-07-25 10:53:40 +0900

    Update debug_photostand_list.php

--- server/apache-php/app/api/debug_photostand_list.php
+++ server/apache-php/app/api/debug_photostand_list.php
@@ -3,9 +3,13 @@
 declare(
     strict_types = 1
 );
-require_once('lib.php');
-$pdo = DBCommon::createConnection();
-var_dump(DBCPhotostand::getAllValue($pdo));
 
+require_once('lib.php');
+
+$pdo = DBCommon::createConnection();
+
+header('Content-type: text/plain');
+
+var_dump(DBCPhotostand::getAllValue($pdo));
 
 
